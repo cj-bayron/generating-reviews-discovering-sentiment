6aaf27b943882c1ef1a2f3d710861b9175daf9b0
cj-bayron <cj.bayron@congrego.co>
2019-04-01 13:18:00 +0800

    add save model feature

--- sst_binary_demo.py
+++ sst_binary_demo.py
@@ -1,21 +1,25 @@
 from encoder import Model
 from matplotlib import pyplot as plt
-from utils import sst_binary, train_with_reg_cv
+from utils import sst_binary, train_with_reg_cv, test_pretrained_clf
 import numpy as np
 
-LOAD_DATA = False
+LOAD_DATA = True
+LOAD_MODEL = True
 
 model = Model()
 
+# data preprocessing
 if LOAD_DATA is True:
-	print('Loading labels...', end='')
-	trY = np.load('data/processed/train_y.npy')
+	print('Loading labels... ', end='')
+	if LOAD_MODEL is False:
+		trY = np.load('data/processed/train_y.npy')
 	vaY = np.load('data/processed/valid_y.npy')
 	teY = np.load('data/processed/test_y.npy')
 	print('Completed.')
 
-	print('Loading inputs...', end='')
-	trXt = np.load('data/processed/train_x.npy')
+	print('Loading inputs... ', end='')
+	if LOAD_MODEL is False:
+		trXt = np.load('data/processed/train_x.npy')
 	vaXt = np.load('data/processed/valid_x.npy') 
 	teXt = np.load('data/processed/test_x.npy')
 	print('Completed.')
@@ -49,18 +53,25 @@ else:
 	np.save('data/processed/test_x.npy', teXt)
 	print('Completed.')
 
-print('Training classifier... ', end='')
-# classification results
-full_rep_acc, c, nnotzero = train_with_reg_cv(trXt, trY, vaXt, vaY, teXt, teY)
-print('Completed.')
+if LOAD_MODEL is True:
+	print('Testing classifier... ', end='')
+	full_rep_acc = test_pretrained_clf(teXt, teY)
+	print('Completed.')
+	print('%05.2f test accuracy'%full_rep_acc)
 
-print('%05.2f test accuracy'%full_rep_acc)
-print('%05.2f regularization coef'%c)
-print('%05d features used'%nnotzero)
+else:
+	print('Training classifier... ', end='')
+	# classification results
+	full_rep_acc, c, nnotzero = train_with_reg_cv(trXt, trY, vaXt, vaY, teXt, teY,
+												  save_model=True)
+	print('Completed.')
+	print('%05.2f test accuracy'%full_rep_acc)
+	print('%05.2f regularization coef'%c)
+	print('%05d features used'%nnotzero)
 
-# visualize sentiment unit
-sentiment_unit = trXt[:, 2388]
-plt.hist(sentiment_unit[trY==0], bins=25, alpha=0.5, label='neg')
-plt.hist(sentiment_unit[trY==1], bins=25, alpha=0.5, label='pos')
-plt.legend()
-plt.show()
+	# visualize sentiment unit
+	sentiment_unit = trXt[:, 2388]
+	plt.hist(sentiment_unit[trY==0], bins=25, alpha=0.5, label='neg')
+	plt.hist(sentiment_unit[trY==1], bins=25, alpha=0.5, label='pos')
+	plt.legend()
+	plt.show()
--- utils.py
+++ utils.py
@@ -1,29 +1,45 @@
 import os
 import html
+import joblib
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tqdm import tqdm
 from sklearn.linear_model import LogisticRegression
 
+CLF_PATH = 'classifier/sst_clf.pkl'
+
 def train_with_reg_cv(trX, trY, vaX, vaY, teX=None, teY=None, penalty='l1',
-        C=2**np.arange(-8, 1).astype(np.float), seed=42):
+        C=2**np.arange(-8, 1).astype(np.float), seed=42, save_model=False):
     scores = []
     for i, c in tqdm(enumerate(C)):
-        model = LogisticRegression(C=c, penalty=penalty, random_state=seed+i)
+        model = LogisticRegression(C=c, penalty=penalty, random_state=seed+i,
+                                   solver='liblinear')
         model.fit(trX, trY)
         score = model.score(vaX, vaY)
         scores.append(score)
+
     c = C[np.argmax(scores)]
-    model = LogisticRegression(C=c, penalty=penalty, random_state=seed+len(C))
+    model = LogisticRegression(C=c, penalty=penalty, random_state=seed+len(C),
+                               solver='liblinear')
     model.fit(trX, trY)
+
+    if save_model:
+        joblib.dump(model, CLF_PATH)
+
     nnotzero = np.sum(model.coef_ != 0)
     if teX is not None and teY is not None:
         score = model.score(teX, teY)*100.
     else:
         score = model.score(vaX, vaY)*100.
+
     return score, c, nnotzero
 
+def test_pretrained_clf(teX, teY):
+    model = joblib.load(CLF_PATH)
+    score = model.score(teX, teY)*100.
+    return score
 
 def load_sst(path):
     data = pd.read_csv(path)
